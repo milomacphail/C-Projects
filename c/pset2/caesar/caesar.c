d3476f527bba199b4d1392965363899340c20131
Milo MacPhail <milomacphail@gmail.com>
2018-11-27 20:50:28 +0000

    Initial function

---
+++ c/pset2/caesar/caesar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main (argc, argv)
+{
+    int k = get_int("Please enter ")
+    do
+    {
+        printf("Please provide acceptable input.");
+        return 1;
+    }
+    while (k != argv[1]);
+
+
+
+
+
+}
